c74fdea77d990b44b0a287daaaa5dc40517ecc85
Julio Gutierrez <bubuntux@gmail.com>
2017-09-03 10:50:18 -0600

    add logic into dockerfile

--- main.sh
+++ main.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-set -e
-
 docker_versions=`curl -sSL 'https://registry.hub.docker.com/v2/repositories/bubuntux/riot-web/tags?page_size=1024' | jq '."results"[]["name"]' | tr -d '"' | sort --version-sort`
 riot_versions=`git ls-remote --tags --refs https://github.com/vector-im/riot-web | awk '{print $2}' | sed 's|^refs/tags/||' | sort --version-sort`
-
-gpg_import () {
-	if [ -z "$gpg_import_flag" ]; then
-		curl -sSl https://riot.im/packages/keys/riot.asc | gpg --import
-		gpg_import_flag=true
-	fi
-}
 
 docker_login () {
 	if [ -z "$docker_login_flag" ]; then
@@ -24,32 +15,10 @@ process_versions () {
 			echo "Version ${riot_version} already in docker."
 	  		continue
 		fi
-
-		echo "Downloading riot-web ${riot_version} app..."
-		curl -sSL https://github.com/vector-im/riot-web/releases/download/${riot_version}/riot-${riot_version}.tar.gz     -o riot-web.tar.gz
-		curl -sSL https://github.com/vector-im/riot-web/releases/download/${riot_version}/riot-${riot_version}.tar.gz.asc -o riot-web.tar.gz.asc
-		
-		echo "Verifying files..."
-		gpg_import
-		gpg --verify riot-web.tar.gz.asc riot-web.tar.gz
-
-		echo "Preparing the app..."
-		tar -xzf riot-web.tar.gz
-		rm riot-web.tar.gz*
-		mv riot-${riot_version} riot-web/
-		cp riot-web/config.sample.json riot-web/config.json
-
-		echo "Building docker image..."
-		docker build -qt bubuntux/riot-web:$riot_version .
-		echo "Pushing docker image..."
+		docker build -t bubuntux/riot-web:$riot_version --build-arg version=$riot_version .
 		docker_login
 		docker push bubuntux/riot-web:$riot_version
-
 		latest_version=$riot_version
-
-		echo "Cleanig up..."
-		rm -fr riot-web
-		echo ""
 	done
 }
 
